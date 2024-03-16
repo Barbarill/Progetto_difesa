5ce50dcf7c6eec085ee67905dbf37b7264d06516
Andrea <andreabarbaro01@gmail.com>
2024-03-16 19:54:02 +0100

    player movement

---
+++ Assets/player_movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public float speed = 5f; // Velocità di movimento del personaggio
+
+    // Update viene chiamato ad ogni frame
+    void Update()
+    {
+        // Ottieni l'input orizzontale e verticale dalle frecce della tastiera
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Calcola la direzione di movimento
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        // Applica il movimento relativo alla velocità e al tempo
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
